e90e970e9ae84353b4606dddd598ead2f692822a
Sir-Alexio <lmokhareva@gmail.com>
2021-09-17 12:55:04 +0300

    feat: add count cats and dream team still in progress

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,7 +13,21 @@ import { NotImplementedError } from '../extensions/index.js';
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function createDreamTeam( members ) {
+  
+  if (typeof(members)!=='object') {
+    return false
+  }
+
+  members.sort()
+  
+  let name = ''
+
+  for (let i = 0; i < members[i]; i++) {
+    if (typeof(members[i]) !== 'string') {
+      continue
+    }
+    name+=members[i][0]
+  }
+  return name
 }
